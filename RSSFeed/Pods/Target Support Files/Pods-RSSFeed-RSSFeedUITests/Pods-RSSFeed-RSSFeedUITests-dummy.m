778377d192e628108f0fbff10103cc115c7c054e
Antun Novosel <novosel.antun@gmail.com>
2025-06-09 08:30:12 +0200

    gitignore update and delete Pods folder

--- RSSFeed/Pods/Target Support Files/Pods-RSSFeed-RSSFeedUITests/Pods-RSSFeed-RSSFeedUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RSSFeed_RSSFeedUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RSSFeed_RSSFeedUITests
-@end
--- RSSFeed/Pods/Target Support Files/Pods-RSSFeed/Pods-RSSFeed-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RSSFeed : NSObject
-@end
-@implementation PodsDummy_Pods_RSSFeed
-@end
